b8acc7f4a2973f4f5ef5779cb48197b73fe4ae80
lucius <luciusyeo18@gmail.com>
2025-08-20 23:03:12 +0800

    Feat: implement echoing of user input for chani

--- src/main/java/Chani.java
+++ src/main/java/Chani.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class Chani {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String msg = "Hello! I'm Chani\n" +
-                     "What can I do for you?\n" +
-                     "Bye. Hope to see you again soon!\n";
-        System.out.println(msg);
+        Scanner scanner = new Scanner(System.in);
+
+        String greeting = "Hello! I'm Chani\n" +
+                "What can I do for you?\n";
+        System.out.println(greeting);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Chani: Bye ;) Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println("Chani: " + input);
+        }
     }
 }
