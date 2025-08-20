894c2b90f3cd5822086d7bc99bff4270bb85fe08
lucius <luciusyeo18@gmail.com>
2025-08-21 03:27:33 +0800

    refactor: use ArrayList to store Tasks

--- src/main/java/Chani.java
+++ src/main/java/Chani.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Chani {
     public static void main(String[] args) {
         final String BOT_NAME = "Chani";
         Scanner scanner = new Scanner(System.in);
 
-        Task[] tasks = new Task[100];
-        int count = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         String greeting = String.format("Hello! I'm %s\nWhat can I do for you?", BOT_NAME);
         System.out.println(greeting);
@@ -18,9 +18,9 @@ public class Chani {
 
             switch(command) {
                 case "list":
-                    for (int i = 0; i < count; i ++) {
+                    for (int i = 0; i < tasks.size(); i ++) {
                         int numbering = i + 1;
-                        System.out.println(numbering + ". " + tasks[i]);
+                        System.out.println(numbering + ". " + tasks.get(i));
                     }
                     break;
                 case "bye":
@@ -28,14 +28,14 @@ public class Chani {
                     return;
                 case "mark": {
                     int taskId = Integer.parseInt(commandRest[1]);
-                    Task taskToMark = tasks[taskId - 1];
+                    Task taskToMark = tasks.get(taskId - 1);
                     taskToMark.markAsDone();
                     System.out.println("Nice! I've marked this task as done:");
                     System.out.println("    " + taskToMark);
                     break;
                 } case "unmark": {
                     int taskId = Integer.parseInt(commandRest[1]);
-                    Task taskToUnMark = tasks[taskId - 1];
+                    Task taskToUnMark = tasks.get(taskId - 1);
                     taskToUnMark.markAsUnDone();
                     System.out.println("Nice! I've marked this task as not done yet:");
                     System.out.println("    " + taskToUnMark);
@@ -48,11 +48,10 @@ public class Chani {
                         }
                         String description = commandRest[1];
                         Task toDo = new ToDos(description);
-                        tasks[count] = toDo;
-                        count++;
+                        tasks.add(toDo);
                         System.out.println("Got it. I've added this task:");
                         System.out.println(toDo);
-                        System.out.println("Now you have " + count + " tasks in the list.");
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     } catch (ChaniException e) {
                         System.out.println("    " + e.getMessage());
                     }
@@ -72,15 +71,13 @@ public class Chani {
                         }
 
                         Task deadline = new Deadline(descBy[0], descBy[1]);
-                    tasks[count] = deadline;
-                    count++;
+                    tasks.add(deadline);
                     System.out.println("Got it. I've added this task:");
                     System.out.println(deadline);
-                    System.out.println("Now you have " + count + " tasks in the list.");
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     } catch (ChaniException e) {
                         System.out.println("    " + e.getMessage());
                     }
-
                     break;
                 } case "event": {
                     String rest = commandRest[1];
@@ -88,11 +85,11 @@ public class Chani {
                     String[] fromTo = descriptionRest[1].split(" /to ", 2);
 
                     Task event = new Event(descriptionRest[0], fromTo[0], fromTo[1]);
-                    tasks[count] = event;
-                    count++;
+                    tasks.add(event);
                     System.out.println("Got it. I've added this task:");
                     System.out.println(event);
-                    System.out.println("Now you have " + count + " tasks in the list.");
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    break;
                 } default:
                     try {
                         throw new ChaniException("OOPS!!! I'm sorry, but I don't know what that means :-(");
