4521d7be429d9e38ab6ec9a3128df0d010a30340
lucius <luciusyeo18@gmail.com>
2025-08-21 02:16:25 +0800

    fix: error caused by string.split limit

--- src/main/java/Chani.java
+++ src/main/java/Chani.java
@@ -13,8 +13,8 @@ public class Chani {
 
         while (true) {
             String input = scanner.nextLine();
-            String[] splitInput = input.split(" ", 2);
-            String command = splitInput[0];
+            String[] commandRest = input.split(" ", 2);
+            String command = commandRest[0];
 
             switch(command) {
                 case "list":
@@ -27,24 +27,51 @@ public class Chani {
                     System.out.println("Bye. Hope to see you again soon!");
                     return;
                 case "mark": {
-                    int taskId = Integer.parseInt(splitInput[1]);
+                    int taskId = Integer.parseInt(commandRest[1]);
                     Task taskToMark = tasks[taskId - 1];
                     taskToMark.markAsDone();
-                    System.out.println("Nice! I've marked this task as done:\n");
+                    System.out.println("Nice! I've marked this task as done:");
                     System.out.println("    " + taskToMark);
                     break;
                 } case "unmark": {
-                    int taskId = Integer.parseInt(splitInput[1]);
+                    int taskId = Integer.parseInt(commandRest[1]);
                     Task taskToUnMark = tasks[taskId - 1];
                     taskToUnMark.markAsUnDone();
-                    System.out.println("Nice! I've marked this task as not done yet:\n");
+                    System.out.println("Nice! I've marked this task as not done yet:");
                     System.out.println("    " + taskToUnMark);
                     break;
-                } default:
-                    String description = splitInput[1];
-                    tasks[count] = new Task(description);
+                } case "todo": {
+                    String description = commandRest[1];
+                    Task toDo = new ToDos(description);
+                    tasks[count] = toDo;
                     count++;
-                    System.out.println("added: " + description);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(toDo);
+                    System.out.println("Now you have " + count + " tasks in the list.");
+                    break;
+                } case "deadline": {
+                    String rest = commandRest[1];
+                    String[] descBy = rest.split(" /by ", 2);
+                    Task deadline = new Deadline(descBy[0], descBy[1]);
+                    tasks[count] = deadline;
+                    count++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(deadline);
+                    System.out.println("Now you have " + count + " tasks in the list.");
+
+                    break;
+                } case "event": {
+                    String rest = commandRest[1];
+                    String[] descriptionRest = rest.split(" /from ", 2);
+                    String[] fromTo = descriptionRest[1].split(" /to ", 2);
+
+                    Task event = new Event(descriptionRest[0], fromTo[0], fromTo[1]);
+                    tasks[count] = event;
+                    count++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(event);
+                    System.out.println("Now you have " + count + " tasks in the list.");
+                } default:
             }
         }
     }
