d3dfb12043afce8bc04bfb7bd8de55eb29da8268
lucius <luciusyeo18@gmail.com>
2025-08-21 01:45:18 +0800

    feat: mark and unmark for Chani

--- src/main/java/Chani.java
+++ src/main/java/Chani.java
@@ -5,7 +5,7 @@ public class Chani {
         final String BOT_NAME = "Chani";
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int count = 0;
 
         String greeting = String.format("Hello! I'm %s\nWhat can I do for you?", BOT_NAME);
@@ -13,7 +13,7 @@ public class Chani {
 
         while (true) {
             String input = scanner.nextLine();
-            String[] splitInput = input.split(" ");
+            String[] splitInput = input.split(" ", 2);
             String command = splitInput[0];
 
             switch(command) {
@@ -26,11 +26,25 @@ public class Chani {
                 case "bye":
                     System.out.println("Bye. Hope to see you again soon!");
                     return;
-                default:
-                    tasks[count] = input;
+                case "mark": {
+                    int taskId = Integer.parseInt(splitInput[1]);
+                    Task taskToMark = tasks[taskId - 1];
+                    taskToMark.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:\n");
+                    System.out.println("    " + taskToMark);
+                    break;
+                } case "unmark": {
+                    int taskId = Integer.parseInt(splitInput[1]);
+                    Task taskToUnMark = tasks[taskId - 1];
+                    taskToUnMark.markAsUnDone();
+                    System.out.println("Nice! I've marked this task as not done yet:\n");
+                    System.out.println("    " + taskToUnMark);
+                    break;
+                } default:
+                    String description = splitInput[1];
+                    tasks[count] = new Task(description);
                     count++;
-                    System.out.println("added: " + input);
-
+                    System.out.println("added: " + description);
             }
         }
     }
