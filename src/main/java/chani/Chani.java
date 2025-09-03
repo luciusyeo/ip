3b823149d6301e879f7f01e2a2067ba953b921a6
lucius <luciusyeo18@gmail.com>
2025-09-03 22:02:57 +0800

    Implement GUI

--- src/main/java/chani/Chani.java
+++ src/main/java/chani/Chani.java
@@ -2,17 +2,28 @@ package chani;
 
 import chani.commands.Command;
 
+/**
+ * Represents the main chatbot class
+ */
 public class Chani {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
 
+    /**
+     * Class Constructor.
+     *
+     * @param fPath txt file path for storage.
+     */
     public Chani(String fPath) {
         this.ui = new Ui("Chani");
         this.storage = new Storage(fPath);
         tasks = new TaskList(storage.load());
     }
 
+    /**
+     * Runs ChaniBot.
+     */
     public void run() {
         ui.showWelcome();
         boolean isExit = false;
@@ -31,6 +42,16 @@ public class Chani {
                 ui.showLine();
             }
         }
+    }
+
+    /**
+     * Returns gui message for Chani bot
+     * @param input text to return.
+     * @return Chani bot's message.
+     * @throws IllegalArgumentException If zone is <= 0.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/chani/gui/MainWindow.java
@@ -0,0 +1,54 @@
+package chani.gui;
+
+import chani.Chani;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Chani chani;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaChani.jpeg"));
+    private Image chaniImage = new Image(this.getClass().getResourceAsStream("/images/DaDerp.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Duke instance */
+    public void setChani(Chani c) {
+        chani = c;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = chani.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, chaniImage)
+        );
+        userInput.clear();
+    }
+}
